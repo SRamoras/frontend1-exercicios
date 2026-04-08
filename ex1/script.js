feef501a4d438c84df362a8b9e4fc835d202d58c
SRamoras <120826220+SRamoras@users.noreply.github.com>
2026-04-08 20:36:49 +0100

    feat: add ex1

---
+++ ex1/script.js
@@ -0,0 +1,13 @@
+let obj = { name: "Diogo Silva", age: 21, city: "Faro" };
+
+let stringifiedObj = JSON.stringify(obj);
+console.log(stringifiedObj);
+
+let parsedObj = JSON.parse(stringifiedObj);
+console.log(parsedObj.name);
+
+let data = fetch("ex1/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
