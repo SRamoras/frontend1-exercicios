4aacaa535b52ac1f7c347e6a268e38ca0ccdc9c2
SRamoras <120826220+SRamoras@users.noreply.github.com>
2026-04-14 21:56:42 +0100

    feat: completed ex4

---
+++ ex4/script.js
@@ -0,0 +1,27 @@
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const result = await response.json();
+  let posts = result.slice(0, 5);
+  
+  let postList = document.querySelector("#post-list");
+
+  posts.forEach((post) => {
+    let postsContainer = document.createElement("div");
+    postsContainer.innerHTML = `
+    <div>
+        <p>${post.userId}</p>
+        <p>${post.id}</p>
+        <p>${post.title}</p>
+        <p>${post.body}</p>
+    </div>
+    `;
+    postList.appendChild(postsContainer);
+  });
+}
+
+let button = document.querySelector("#get-data");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  getData();
+});
